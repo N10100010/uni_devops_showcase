b354def697eac3d293debfce3e639445e367b389
Nick <nr034@hdm-stuttgart.de>
2022-06-19 15:32:57 +0200

    [CONFIG] - finalized local test

--- tests_local/test_container.py
+++ tests_local/test_container.py
@@ -5,5 +5,6 @@ import urllib.request
 # TEST THE ACTIVE CONTAINER
 ###########################
 def test_active_container():
+    # docker-compose up before that
     contents = urllib.request.urlopen("http://localhost:10000/").read()
     assert b"Feel welcomed to the greatest of all FizzBuzzes" in contents
